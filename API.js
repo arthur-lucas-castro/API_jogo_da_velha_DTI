cbe4308374159050db7ce2819cc59a30cc431cb8
arthur-lucas-castro <arthurcastro1117@gmail.com>
2019-12-10 10:42:01 -0200

    JSON gerado

--- API.js
+++ API.js
@@ -1,6 +1,24 @@
 exports.criarJogo = 
 function criarJogo(){
-  console.log("jogo criado");
+    var id = create_UUID();
+    var jogador = ['X','O'];
+    let indexSorteado = Math.floor(Math.random() * 2);
+    var firstPlayer= jogador[indexSorteado];
+  var json ={
+      'id': id,
+      'firstPlayer': firstPlayer
+
+  };
+  console.log(json);
 
 
+}
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 }
