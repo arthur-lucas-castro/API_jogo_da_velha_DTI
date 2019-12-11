6f6875ac71703bf875a2265c31575813da8a0bd4
arthur-lucas-castro <arthurcastro1117@gmail.com>
2019-12-11 17:11:13 -0200

    codigo flnalizado e comentado 1.0

--- API.js
+++ API.js
@@ -2,85 +2,92 @@ var tabuleiro;
 var proximoJogador;
 var Partidas={};
 
-exports.criarJogo = 
-function criarJogo(){
-    var id = create_UUID();
-    tabuleiro = [
+exports.CriarJogo = //exporta a funçao CriarJogo pelo modulo Nodejs 
+function CriarJogo(){
+    var id = create_UUID();//chama a funçao que cria um id no formato UUID
+    tabuleiro = [//Cria um tabuleiro
         ["", "", ""],
         ["", "", ""],
         ["", "", ""]
     ];
-    var jogador = ['X','O'];
-    let indexSorteado = Math.floor(Math.random() * 2);
-    proximoJogador= jogador[indexSorteado];
-    var json ={
-        'id': id,
-        'firstPlayer': proximoJogador
+    var jogador = ['X','O'];//possibilidade dos jogadores
+    let indexSorteado = Math.floor(Math.random() * 2);//sorteia um numero para saber qual jogador começa
+    proximoJogador= jogador[indexSorteado];// o numero é passado para o index da variavel jogador, 
+    var json ={//cria um Json com o retorno
+        'id': id,//retorna o id gerado
+        'firstPlayer': proximoJogador// e o primeiro jogador
     };
-    console.log(id);
-    Partidas[id]= tabuleiro;
-    return json;
+    Partidas[id]= tabuleiro;//adiciona na variavel Partidas essa partida que acabou de ser criada
+    return json;//retorna o Json
 }
 
-exports.movimentar =
-function movimentar(id,jogada ){
+exports.Movimentar =//exporta a funçao Movimentar pelo modulo Nodejs 
+function Movimentar(id,jogada ){
     var resultado;
-    if(Partidas[id.id]){
-        var jogada =JSON.parse(jogada);       
-        if(jogada.player==proximoJogador){
+    if(Partidas[id]){//verifica se existe a partida com o id passado
+        var jogada =JSON.parse(jogada);  // transforma o string de jogada em um Json     
+        if(jogada.player==proximoJogador){// caso seja a vez do jogador, faça:
+            //recebe os movimentos
             var x= jogada.position.x;
             var y=jogada.position.y;
+            //inverte o tabuleiro, para que a possiçao 0,0 seja a extrema inferior esquerdo como pedido no enunciado
             if(jogada.position.y==0){
                 y=2;
             }else if(jogada.position.y==2){
                 y=0;
             }
+            //percorre o tabuleiro
             for(var i = 2; i>=y;i--){                
                 for(var j=0; j<x;j++){
                 }
-                if(i==y){
-                  if(Partidas[id.id][i][j]==""){
-                    Partidas[id.id][i][j]=proximoJogador
-                   resultado=Checar_Vitoria(Partidas[id.id],proximoJogador );
-                  }else{
-                    return Enviar_MSG("Invalido");
+                if(i==y){//quando encontrar a casa correta do tabuleiro, faça
+                  if(Partidas[id][i][j]==""){//verifica se a casa esta vazia, caso estiver, faça
+                    Partidas[id][i][j]=proximoJogador//recebe o valor do jogador X ou O
+                   resultado=Checar_Vitoria(Partidas[id.id],proximoJogador );// checa se ouve vitoria chamando a funçao Checar_Vitoria, seu retorno vai para a variavel resultado
+                  }else{//caso a casa nao esteja disponivel, faça
+                    return Enviar_MSG("Invalido");// chama a funçao responsavel por enviar a mensagem para o usuario
                   }
                 }   
-            }
+            } 
+            //verifica qual jogador fez o movimento e entao altera para o proximo
          if(proximoJogador=='X'){
              proximoJogador='O';
-             console.log(resultado)
-             return Enviar_MSG(resultado);
+             return Enviar_MSG(resultado);//envia mensagem para o cliente com o resultado da jogada
          }else{
              proximoJogador='X';
              console.log(resultado)
-             return Enviar_MSG(resultado);
+             return Enviar_MSG(resultado);//envia mensagem para o cliente com o resultado da jogada
          }
-        }else{
+        }else{//caso nao seja o turno do usuario, envie para o cliente a mensagem 
             return Enviar_MSG('Turno errado');
         }
-    }else{
+    }else{//caso nao exista a partida envia a mensagem para o cliente
         return Enviar_MSG("Partida Inexistente");
     }
 }
-function Checar_Vitoria(tabuleiro,jogador){
+function Checar_Vitoria(tabuleiro,jogador){//funçao responsavel por verificar se ouve vitoria, recebe por parametro um tabuleiro e o jogador que fez a jogada
     var draw=1;
+    //verifica se ouve vencedor na horizontal e retorna o vencedor 
     for(var i=0; i<3; i++){
         if(tabuleiro[i][0]==tabuleiro[i][1] && tabuleiro[i][2] == tabuleiro[i][0] && (tabuleiro[i][0]=='X'||tabuleiro[i][0]=='O')){
             return jogador;
         }
     }
+    //verifica se ouve vencedor na vertical e retorna o vencedor 
     for(var i=0; i<3; i++){       
         if(tabuleiro[0][i]==tabuleiro[1][i] && tabuleiro[2][i] == tabuleiro[0][i] && (tabuleiro[0][i]=='X'|| tabuleiro[0][i]=='O')){
             return jogador;
         }
     }
+    //verifica se ouve vencedor na diagonal secundaria e retorna o vencedor 
     if(tabuleiro[0][0]==tabuleiro[1][1] && tabuleiro[2][2] == tabuleiro[0][0] && (tabuleiro[0][0]=='X'|| tabuleiro[0][0]=='O')){
         return jogador;
     }
+    //verifica se ouve vencedor na diagonal primaria e retorna o vencedor 
     if(tabuleiro[2][0]==tabuleiro[1][1] && tabuleiro[0][2] == tabuleiro[1][1] && (tabuleiro[1][1]=='X'|| tabuleiro[1][1]=='O')){
         return jogador;
     }
+    //verifica se existe algum quadrado vazio, caso tenha a variavel draw recebera 0
     for(var i=0; i <3; i++){
         for(var j=0; j <3; j++){
             if(tabuleiro[i][j]==""){
@@ -88,14 +95,16 @@ function Checar_Vitoria(tabuleiro,jogador){
             }
         }
     }
+//caso variavel draw tenha o valor 1 quer dizer q ocorreu um empate
     if(draw==1){
         return "Draw";
-    }else{
+    }else{//caso nao tenha ocorrido nenhuma dessas condiçoes anteriores, a jogada apenas foi valida
         return 200;
     }
 
 }
-function Enviar_MSG(estado_do_Jogo){
+function Enviar_MSG(estado_do_Jogo){//funçao para enviar a mensagem
+    //retorna um json com as mensagens de acordo com o estado do jogo
     var json={};
     if(estado_do_Jogo=='Turno errado'){
         json={
@@ -137,7 +146,8 @@ function Enviar_MSG(estado_do_Jogo){
         return json;
     }
 }
-function create_UUID(){
+// funçao para criar um UUID, id unico universal
+function create_UUID(){//funçao publica, disponivel no site https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
     var dt = new Date().getTime();
     var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = (dt + Math.random()*16)%16 | 0;
--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-var api = require('./API.js');
-const url = require('url');
-var json;
-http.listen(3000, function(){
-    console.log("porta 3000");
+//server Node.JS
+const app = require('express')()//requerindo a biblioteca esxpress.js
+const http = require('http').createServer(app)//criando um server local com o express
+var api = require('./API.js');//requerindo a API criada
+http.listen(3000, function(){//iniciando o server na porta 3000
+    console.log("server iniciado na porta 3000");
 });
-app.get('/', (req, res)=>{
+app.get('/', (req, res)=>{//quando abrir o localhost:3000 enviar para a pagina index.html
     res.sendFile(__dirname + '/index.html');
 })
-app.get('/game', (req, res)=>{
- json = api.criarJogo();
- res.sendFile(__dirname + '/index.html');
- res.send(json);
+app.get('/game', (req, res)=>{//quando tiver uma requisição no localhost:3000/game, faça:
+    res.send(api.CriarJogo());// chama a funçao CriaJogo da API e o retorno dessa funçao é enviado para a tela
 })
-app.get('/game/:id', (req, res)=>{
-    let params;
-    var parametros =  Object.keys(req.query);
-
-    var id =  req.params;
-    res.sendFile(__dirname + '/index.html');
-    res.send(api.movimentar(id,parametros.toString()));
-
-   
-   })
+app.get('/game/:id', (req, res)=>{////quando tiver uma requisição no localhost:3000/game/(um id de uma partida), faça:
+    var parametros =  Object.keys(req.query);//pega os parametros via URL
+    var id =  req.params;// pega o ID da partida
+    res.send(api.Movimentar(id.id,parametros.toString()));//chama a funçao Movimentar da API, com o id e os parametros em forma de string, e o retorno dessa funçao é enviado para a tela
+})
    
