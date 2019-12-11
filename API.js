37032faa87d8b9e7b817367e29ae3282c7e07d9d
arthur-lucas-castro <arthurcastro1117@gmail.com>
2019-12-11 15:07:16 -0200

    detectando vencedor

--- API.js
+++ API.js
@@ -7,9 +7,9 @@ function criarJogo(){
     var id = create_UUID();
    
     tabuleiro = [
-        ["1", "2", "3"],
-        ["4", "5", "6"],
-        ["7", "8", "9"]
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
     ];
         
    
@@ -40,8 +40,48 @@ function movimentar(id,jogada ){
         jogo_Tabuleiro =Partidas[id.id];
         console.log(jogo_Tabuleiro);
         var jogada =JSON.parse(jogada);
-        console.log(tabuleiro[1][2]);
+       
        if(jogada.player==proximoJogador){
+            var x= jogada.position.x;
+            var y=jogada.position.y;
+            if(jogada.position.y==0){
+            y=2;
+
+            }else if(jogada.position.y==2){
+                y=0;
+            }
+           
+           
+            for(var i = 2; i>=y;i--){
+                
+                for(var j=0; j<x;j++){
+                
+                        console.log(i,j);
+                    
+                   
+                }
+                if(i==y){
+                  if(Partidas[id.id][i][j]==""){
+                    console.log("entrou");
+                    Partidas[id.id][i][j]=proximoJogador
+                    console.log( Partidas[id.id]);
+                    Checar_Vitoria(Partidas[id.id]);
+                  }else{
+                      console.log("Jogada invalida");
+                  }
+                }
+               
+                
+            }
+
+
+            
+
+
+
+
+
+
           
          console.log("realizar jogada");
          if(proximoJogador=='X'){
@@ -65,6 +105,61 @@ function movimentar(id,jogada ){
     
 
 
+}
+function Checar_Vitoria(tabuleiro){
+    console.log("checando")
+for(var i=0; i<3; i++){
+    console.log(tabuleiro[i][0]);
+    console.log(tabuleiro[i][1]);
+    console.log(tabuleiro[i][2]);
+        if(tabuleiro[i][0]==tabuleiro[i][1] && tabuleiro[i][2] == tabuleiro[i][0]){
+            console.log("Win");
+            return 1;
+        }
+    
+}
+for(var i=0; i<3; i++){
+    
+    if(tabuleiro[0][i]==tabuleiro[1][i] && tabuleiro[2][i] == tabuleiro[0][i]){
+        console.log("Win");
+        return 1;
+    }
+
+}
+if(tabuleiro[0][0]==tabuleiro[1][1] && tabuleiro[2][2] == tabuleiro[0][0]){
+    console.log("win");
+    return 1;
+}
+if(tabuleiro[2][0]==tabuleiro[1][1] && tabuleiro[0][2] == tabuleiro[1][1]){
+    console.log("win");
+    return 1;
+}
+var draw=1;
+for(var i; i <3; i++){
+    for(var j; j <3; j++){
+        if(tabuleiro[i][j]==""){
+        draw=0;
+        }
+    }
+}
+if(draw==1){
+    return 0;
+}else{
+    return 2;
+}
+
+}
+function Enviar_MSG(estado_do_Jogo){
+   /* if(estado_do_Jogo=='Turno errado'){
+        
+    }else if(){
+
+    }else if(){
+
+    }else(){
+
+    }
+*/
 }
 function create_UUID(){
     var dt = new Date().getTime();
